41ef63ef5025f66fa4d167ecd36f198006c54072
datahiti <107811601+datahiti@users.noreply.github.com>
2022-09-21 19:46:04 -1000

    ajout contrôle des couches

--- script.js
+++ script.js
@@ -3,11 +3,22 @@ async function drawMap()
   var map = L.map('map').setView([-21.443557, 165.559522], 8);
 
   var tiles = L.tileLayer(
-	'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-	maxZoom: 18
-	}).addTo(map);
+    'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 18
+    }).addTo(map);
 
-    
+   var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+     maxZoom: 19,
+     attribution: '© OpenStreetMap'
+   });
+
+  var baseMaps = {
+    "OpenStreetMap": osm,
+    "Satellite": tiles
+  };
+
+  var layerControl = L.control.layers(baseMaps).addTo(map);
+
   function onEachFeature(feature, layer) {
     if (feature.properties && feature.properties.popupContent) {
       layer.bindPopup(feature.properties.popupContent);
